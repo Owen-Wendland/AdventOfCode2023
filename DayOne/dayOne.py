439561e296907ecb6a8562beb389eb74361c2180
Owen_Wendland <owsun21@gmail.com>
2023-12-02 23:54:38 -0800

    Day one completed!

---
+++ DayOne/dayOne.py
@@ -0,0 +1,28 @@
+#Day one of 2023 Advent - Owen 
+endResult = 0
+first = False
+firstNum = 0
+secondNum = 0
+
+f = open("dayOne.txt", "r")
+
+for a in f:
+    
+    for i in range(len(a)): 
+        
+        if(a[i].isnumeric() == True and first == False):
+            firstNum = a[i]
+            secondNum = firstNum
+            first = True
+            
+        elif(a[i].isnumeric()):
+            secondNum = a[i]
+    
+    output = int(firstNum + secondNum)
+    endResult = endResult + output
+    first = False
+    
+print(endResult)
+
+f.close()
+quit() 
